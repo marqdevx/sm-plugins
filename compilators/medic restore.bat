56c5ddb22631347e5a5c151ff3ec751040490a50
marqdevx <marqdevx@users.noreply.github.com>
2023-02-07 18:36:03 +0100

    Add compilers

---
+++ compilators/medic restore.bat
@@ -0,0 +1,8 @@
+copy ..\sm-medic\csgo_medic.sp ..\..\csgo-sourcemod-versions\sourcemod-1.9.0-git6282-windows\addons\sourcemod\scripting\csgo_medic.sp
+
+cd ..\..\csgo-sourcemod-versions\sourcemod-1.9.0-git6282-windows\addons\sourcemod\scripting\
+
+spcomp.exe csgo_medic.sp
+
+del csgo_medic.sp
+move csgo_medic.smx ..\..\..\..\..\sm-plugins\sm-medic
